f192a669247e457dbd4269e2905c658e18c31d38
T-Gamer <1985michel>
2020-09-01 09:57:06 -0300

    começando

---
+++ app/src/main/java/com/example/kissmoney/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.kissmoney
+
+import android.content.Context
+import android.graphics.Rect
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+    }
+
+    //ocultando o teclado ao clicar fora
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+
+        //O método abaixo oculta o teclado sempre que clicamos fora do editText
+        //tem que colocar essa linha no layout (root) da view(xml):
+        //android:focusableInTouchMode="true"
+
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
+}
