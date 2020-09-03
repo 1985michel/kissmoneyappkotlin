1deee1ce37cb5e00e96cba646adc793a70046622
T-Gamer <1985michel>
2020-09-03 08:21:43 -0300

    corrigido nome da aplicação no gradle

--- app/src/main/java/com/example/kissmoney/mes/mesFragment.kt
+++ app/src/main/java/com/example/kissmoney/mes/mesFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentTransaction
 import com.example.kissmoney.R
+import com.example.kissmoney.databinding.FragmentChatBinding
+import com.example.kissmoney.databinding.FragmentMesBinding
+import com.example.kissmoney.interacao.chatFragment
+import kotlinx.android.synthetic.main.fragment_mes.*
 
 
 class mesFragment : Fragment() {
@@ -16,7 +22,45 @@ class mesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mes, container, false)
+
+
+        val binding = DataBindingUtil.inflate<FragmentMesBinding>(
+            inflater,
+            R.layout.fragment_mes,
+            container,
+            false
+        )
+
+        binding.progressBarDiasDeLiberdade.setOnClickListener {
+            binding.textoCentralTextView.text = "125 Dias de Liberdade.\n Meta: 180 dias"
+        }
+
+        binding.progressBarGastoNoMes.setOnClickListener {
+            binding.textoCentralTextView.text = "Já foram gastos 76% do que foi ganho no mês."
+        }
+
+        binding.progressBarPoupadoNoMes.setOnClickListener {
+            binding.textoCentralTextView.text = "Você investiu 10% da sua renda. Parabéns!"
+        }
+
+        binding.buttonGreen.setOnClickListener {
+            binding.textoCentralTextView.text = "125 Dias de Liberdade.\n Meta: 180 dias"
+        }
+
+        binding.buttonRed.setOnClickListener {
+            binding.textoCentralTextView.text = "Já foram gastos 76% do que foi ganho no mês."
+        }
+
+        binding.buttonGrey.setOnClickListener {
+            binding.textoCentralTextView.text = "Você investiu 10% da sua renda. Parabéns!"
+        }
+
+
+        return binding.root
+
+        //return inflater.inflate(R.layout.fragment_mes, container, false)
+
+
     }
 
 }
